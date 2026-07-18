ea2b2baa941c9e4cee5b2691f7aaffbfda4c7c05
Amritraj7870 <amritraj692@gmail.com>
2026-07-18 23:04:59 +0530

    even numbers written

--- index.js
+++ index.js
@@ -21,3 +21,17 @@ console.log("server is running on port 4000")
 })
 
 
+// printing the even numbers from 1 to 10
+
+for(let num=1;num<=10;num++){
+    let count=0;
+    for(let i=2;i<num;i++){
+        if(num%i==0){
+            count++
+        }
+    }
+    if(count==0 && num>1){
+        console.log(num)
+    }
+}
+
